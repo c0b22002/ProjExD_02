7b10520513f4b33baf5ac717a8f42ce34a40a9b8
c0b002 <c0b22002a6@edu.teu.ac.jp>
2023-06-20 14:42:01 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,6 +12,8 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
     bomb_img = pg.Surface((20, 20))
     pg.draw.circle(bomb_img, (255, 0, 0), (10, 10), 10)
     bomb_x = random.randint(0, WIDTH)
@@ -20,15 +22,30 @@ def main():
     bomb_rct = bomb_img.get_rect()
     bomb_rct.center = bomb_x, bomb_y  
     vx, vy = 5, 5
+    gokk = {
+        pg.K_UP: (0, -5),
+        pg.K_DOWN: (0, 5),
+        pg.K_LEFT: (-5, 0),
+        pg.K_RIGHT: (5, 0),
+    }
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
 
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
+        for k, mv in gokk.items():
+            if key_lst[k]:
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv)
+
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img, kk_rct)
         bomb_rct.move_ip(vx, vy)
         screen.blit(bomb_img, bomb_rct)
         pg.display.update()
